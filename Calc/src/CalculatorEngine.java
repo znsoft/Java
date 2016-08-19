7cf4ee9f00ba2481a2d83b27e1c31bb229d5d309
Tkalin Vladimir <tkalin.vladimir@gmail.com>
2016-08-19 23:33:30 +1000

    надо сделать exit

--- Calc/src/CalculatorEngine.java
+++ Calc/src/CalculatorEngine.java
@@ -1,8 +1,9 @@
-import java.awt.event.ActionListener;
+import javax.swing.*;
 import java.awt.event.ActionEvent;
-import javax.swing.JButton;
+import java.awt.event.ActionListener;
 
-public class CalculatorEngine implements ActionListener {
+public class CalculatorEngine implements ActionListener
+{
 
     Calculator parent; //ссылка на окно калькулятора
     char selectedAction = ' '; // +, -, /, или *
@@ -10,26 +11,28 @@ public class CalculatorEngine implements ActionListener {
 
     // Конструктор сохраняет ссылку на окно калькулятора
 // в переменной экземпляра класса
-    CalculatorEngine(Calculator parent) {
+    CalculatorEngine(Calculator parent)
+    {
         this.parent = parent;
     }
 
-    public void actionPerformed(ActionEvent e) {
-// Получить источник действия
+    public void actionPerformed(ActionEvent e)
+    {
+        // Получить источник действия
         JButton clickedButton = (JButton) e.getSource();
         String dispFieldText = parent.displayField.getText();
         double displayValue = 0;
-// Получить число из дисплея калькулятора,
-// если он не пустой.
-// Восклицательный знак – это оператор отрицания
-        if (!"".equals(dispFieldText)) {
+        // Получить число из дисплея калькулятора,
+        // если он не пустой.
+        // Восклицательный знак – это оператор отрицания
+        if (!dispFieldText.equals("")) {
             displayValue = Double.parseDouble(dispFieldText);
         }
         Object src = e.getSource();
-// Для каждой кнопки арифметического действия
-// запомнить его: +, -, /, или *, сохранить текущее число
-// в переменной currentResult, и очистить дисплей
-// для ввода нового числа
+        // Для каждой кнопки арифметического действия
+        // запомнить его: +, -, /, или *, сохранить текущее число
+        // в переменной currentResult, и очистить дисплей
+        // для ввода нового числа
         if (src == parent.buttonPlus) {
             selectedAction = '+';
             currentResult = displayValue;
@@ -48,12 +51,12 @@ public class CalculatorEngine implements ActionListener {
             parent.displayField.setText("");
         } else if (src == parent.buttonEqual) {
             // Совершить арифметическое действие, в зависимости
-// от selectedAction, обновить переменную currentResult
-// и показать результат на дисплее
+            // от selectedAction, обновить переменную currentResult
+            // и показать результат на дисплее
             if (selectedAction == '+') {
                 currentResult += displayValue;
-// Сконвертировать результат в строку, добавляя его
-// к пустой строке и показать его
+                // Сконвертировать результат в строку, добавляя его
+                // к пустой строке и показать его
                 parent.displayField.setText("" + currentResult);
             } else if (selectedAction == '-') {
                 currentResult -= displayValue;
@@ -66,8 +69,8 @@ public class CalculatorEngine implements ActionListener {
                 parent.displayField.setText("" + currentResult);
             }
         } else {
-// Для всех цифровых кнопок присоединить надпись на
-// кнопке к надписи в дисплее
+        // Для всех цифровых кнопок присоединить надпись на
+        // кнопке к надписи в дисплее
             String clickedButtonLabel = clickedButton.getText();
             parent.displayField.setText(dispFieldText +
                     clickedButtonLabel);
