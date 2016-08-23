3795175b841d195207f7b20c26150982dd36cdcb
Ткалин Владимир <tkalin.vladimir@gmail.com>
2016-08-23 16:38:14 +1000

    крестики-нолики. Разобрать. Вшить в страничку html

--- TikakToe/src/HelloApplet.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by va.tkalin on 23.08.2016.
- */
-public class HelloApplet extends java.applet.Applet {
-    public void paint(java.awt.Graphics graphics) {
-        graphics.drawString("Привет, Мир!", 70, 40);
-    }
-}
