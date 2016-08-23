37ea8d4cf68ae3812fdfa319628f56e95c37d526
Ткалин Владимир <tkalin.vladimir@gmail.com>
2016-08-23 16:11:09 +1000

    Hello World апплет работает.

---
+++ TikakToe/src/HelloApplet.java
@@ -0,0 +1,8 @@
+/**
+ * Created by va.tkalin on 23.08.2016.
+ */
+public class HelloApplet extends java.applet.Applet {
+    public void paint(java.awt.Graphics graphics) {
+        graphics.drawString("Привет, Мир!", 70, 40);
+    }
+}
