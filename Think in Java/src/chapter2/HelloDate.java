eab9a1f6a52331bba92d4fe06c1cdb33cf31d9b7
Ткалин Владимир <tkalin.vladimir@gmail.com>
2016-09-06 16:50:38 +1000

    ПРимеры из Think in Java - дата и окружение.

---
+++ Think in Java/src/chapter2/HelloDate.java
@@ -0,0 +1,14 @@
+package chapter2;
+import java.util.*;
+
+/**
+ * Created by va.tkalin on 06.09.2016.
+ */
+public class HelloDate
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, now:");
+        System.out.println(new Date());
+    }
+}
---
+++ Think in Java/src/chapter2/ShowProperties.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+//: object/ShowProperties.java
+
+public class ShowProperties
+{
+    public static void main(String[] args)
+    {
+        System.getProperties().list(System.out);
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(System.getProperty("java.library.path"));
+    }
+} ///:~
+
+
