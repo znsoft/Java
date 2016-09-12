da519fec9c54033c6168f25c8a1f208c5588530a
Ткалин Владимир <tkalin.vladimir@gmail.com>
2016-09-12 16:58:31 +1000

    Подключили *.java как библиотеку. Юзаем просто принт вместо систем.аут.принтл.

---
+++ TIJ_3/src/HelloDate.java
@@ -0,0 +1,19 @@
+/**
+ * Created by va.tkalin on 12.09.2016.
+ */
+import java.util.Date;
+
+import static net.Print.*;
+
+public class HelloDate {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello, now:");
+        System.out.println(new Date());
+        print("Hello, now:");
+        print(new Date());
+
+    }
+
+}
